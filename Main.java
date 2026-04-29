6be4e7bf1a4e459b73b5b5de154f98d651ac4d2a
Jorge Uziel <jorge.castro82@uabc.edu.mx>
2026-04-29 10:48:13 -0700

    gestor Mascota

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args){
-        
+        Documento[] my_docs = {
+            new Contrato(),
+            new Factura(),
+            new EstructuraPublica(),
+            new EstructuraPropiedad(),    
+        };
     }
 }
