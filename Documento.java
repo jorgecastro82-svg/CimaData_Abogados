0f395922f1d741e11bebc4c9b204ee263fbd6cf2
Daniri98 <daniel.rivera98@uabc.edu.mx>
2026-04-29 17:50:55 +0000

    xdd

--- Documento.java
+++ Documento.java
@@ -3,7 +3,12 @@ public abstract class Documento{
     String nombre_autor;
     String titulo;
     int costo_auditoria = 50;
+    Documento(int id,String nom,String tit){
+        this.id=id;
+        nombre_autor=nom;
+        titulo=tit;
 
+    }
     public abstract void disp_info();
     public abstract boolean generar_reporte();
 }
